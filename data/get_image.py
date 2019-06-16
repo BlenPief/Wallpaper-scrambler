8658b9628ba01bc9abca2d89a8001dac4186884b
BlenPief <lindved.william@gmail.com>
2019-06-16 14:46:53 +0200

    First commit. Prints termination messages

---
+++ data/get_image.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+import urllib2
+import os
+import json
+import random
+import time
+import sys
+
+def get_soup(url,header):
+    return BeautifulSoup(urllib2.urlopen(urllib2.Request(url,headers=header)),'html.parser')
+
+def main():
+
+    WORKDIR = sys.argv[1] 
+
+    dic = open(WORKDIR + "/dictionary.wl", 'r') #Path of the used dictionary
+    dick = dic.readlines()
+
+    impossibility =  random.choice([1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                    2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                                    2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
+                                    4, 4, 4, 4, 5, 5, 6, 7, 8])
+    
+    
+    words = random.sample(dick, impossibility)
+
+    query = ('+'.join(words)).replace("\n", '')
+    image_type = (query.replace("'",'')).replace("+", '_')  #Name of the picture files
+    url= "https://www.google.co.uk/search?q=" + query +\
+         "&source=lnms&tbm=isch#q=" + query + "&tbs=isz:lt,islt:2mp&tbm=isch"
+
+
+    #add the directory for your image here
+    DIR= WORKDIR + "/backgrounds" #Directory for the pictures
+    
+
+    header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
+    }
+    soup = get_soup(url,header)
+
+    
+    ActualImages=[]# contains the link for Large original images, type of  image
+    for a in soup.find_all("div",{"class":"rg_meta"}):
+        link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
+        ActualImages.append((link,Type))
+
+
+
+    ###print images
+    for i , (img , Type) in enumerate( ActualImages[:1]):
+        try:
+            req = urllib2.Request(img, headers={'User-Agent' : header})
+            raw_img = urllib2.urlopen(req).read()
+
+            cntr = len([i for i in os.listdir(DIR) if image_type in i]) + 1
+            if len(Type)==0:
+                f = open(os.path.join(DIR , image_type + ".jpg"), 'wb')
+                pic_path = "file://" + DIR + '/' + image_type + ".jpg"
+            else :
+                f = open(os.path.join(DIR , image_type + "."+Type), 'wb')
+                pic_path = "file://" + DIR + '/'  + image_type + "." + Type
+
+            f.write(raw_img)
+            f.close()
+        except Exception as e:
+            print "could not load : "+img
+            print e
+
+
+   
+    # Create temporary file which stores the picture path
+    f = open(WORKDIR + "/tempfile.wl", 'w')
+    f.write(pic_path[7:])
+    f.close()
+
+main()
---
+++ scramble.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+# Sets random pictures as background
+
+
+#UTILITIES
+
+DIR="$(readlink -f `dirname $0`)/data"
+
+download_pic () {
+	# Download picture and write path in temporary file
+
+	python $DIR/get_image.py $DIR
+	echo `cat $DIR/tempfile.wl`
+	rm $DIR/tempfile.wl
+}
+
+set_wallpaper () {
+	
+	/usr/bin/gsettings set org.gnome.desktop.background picture-uri $1
+
+}
+
+
+random_file () {
+	# Given a directory prints a random file
+
+	local DIR="$1"
+
+	  
+	IFS='
+	'
+	if [[ -d "${DIR}" ]]
+	then
+		local files=($(ls "${DIR}"))
+		local num_files=${#files[*]}
+		echo ${DIR}/${files[$((RANDOM%num_files))]}
+	fi
+}
+#MODES
+
+newpics () {
+	#Downloads random picture from internet and sets as background
+
+	echo -e "Fetching pictures online. Press any key to exit."
+	echo "Current wallpaper is:"
+
+	while [ true ]
+	do
+		local PICPATH=$(download_pic)
+		local PICNAME="${PICPATH##*/}"
+		set_wallpaper $PICPATH
+		echo -ne "\r$PICNAME                                              "
+		sleep $1
+	done
+
+}
+
+
+oldpics () {
+	#Chooses a random wallpaper from downloaded pictures
+
+	echo -e "Using pictures from library. Press any key to exit."
+        echo "Current wallpaper is:"
+
+	while [ true ]; do
+
+        	local PICPATH=$(random_file "$DIR/backgrounds")
+		local PICNAME="${PICPATH##*/}"
+                set_wallpaper "$PICPATH"
+                echo -ne "\r$PICNAME                                            "
+                sleep $1
+        done
+        read -n 1 -s
+	bool=false
+
+	echo -e "\n"
+}
+
+
+#Menu
+main () {
+
+	TIMER=30
+
+	echo -e "Welcome to the scrambler!\n\n"
+
+	while true; do
+		read -p "Would you like [n]ew pictures, [o]ld pictures, change the [t]imer, or [e]xit? " -n1 -s choice
+		echo -e "\n"
+		case $choice in
+			[nN]* ) newpics $(echo "scale=2;60*$TIMER" | bc) &
+				read -n1 -s; kill $!; echo -e "\n";;
+			[oO]* ) oldpics $(echo "scale=2;60*$TIMER" | bc) &
+			       	read -n1 -s; kill $!; echo -e "\n";;
+			[tT]* ) echo "The timer is currently set to $TIMER minutes."
+				read -p "Set the timer:  " TIMER; echo;;
+			[eEqQ]* ) break;;
+			* ) echo -e "\nPlease, choose one of the options!\n";;
+		esac
+	done
+
+	echo -e "\nThank you for scrambling today!"
+
+}
+
+
+main
+	
+
+
+
+
